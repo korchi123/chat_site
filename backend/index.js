6c97c737395ff95d39d408d19b11eee0f2a96587
Alexandr <alexruskor@gmail.com>
2025-08-25 23:39:27 +0300

    1

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,9 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 app.use(ErrorHandlingMiddleware);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 const start = async ()=>{
     try{
