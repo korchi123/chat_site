59d775f66f2757b1fe9806b2dc1c2c606718789d
Alexandr <alexruskor@gmail.com>
2025-08-28 12:02:14 +0300

    a

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import ErrorHandlingMiddleware from './middleware.js/ErrorHandlingMiddleware.js'
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import changePhotoToText from './change-photo-to-text.js';
+
 const app = express();
 const PORT=process.env.PORT || 5000
 // Получаем текущий путь к файлу
